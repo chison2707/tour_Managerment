9d7ff185fd3e3208e100561984c9fec21505bf3b
chison2707 <chiphansonzz17@gmail.com>
2024-12-30 22:15:53 +0700

    làm danh sách tour theo danh mục

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 const router: Router = Router();
 import * as controller from "../../controllers/client/tour.controller";
 
-router.get('/', controller.index);
+router.get('/:slugCategory', controller.index);
 
 export const tourRoutes: Router = router;
