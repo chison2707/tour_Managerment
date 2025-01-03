2f9ee9bf833b33c84ccc06c2af4f142391fd1e91
chison2707 <chiphansonzz17@gmail.com>
2025-01-03 14:13:48 +0700

    HIển thị thông báo đã thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -16,6 +16,19 @@ var swiper2 = new Swiper(".mySwiper2", {
 });
 // End swiper-images-main
 
+// alert success
+const alertAddCartSusscess = () => {
+    const elementAlert = document.querySelector("[alert-add-cart-susscess]");
+    if (elementAlert) {
+        elementAlert.classList.remove("alert-hidden");
+
+        setTimeout(() => {
+            elementAlert.classList.add("alert-hidden");
+        }, 3000);
+    }
+}
+//end  alert success
+
 // cart
 const cart = localStorage.getItem("cart");
 if (!cart) {
@@ -44,6 +57,8 @@ if (formAddToCart) {
 
 
             localStorage.setItem("cart", JSON.stringify(cart));
+
+            alertAddCartSusscess();
         }
     })
 }
