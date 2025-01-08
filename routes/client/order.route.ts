0015dcd1c7fc36346f2f0e1f4d93f331c7d3dac1
chison2707 <chiphansonzz17@gmail.com>
2025-01-08 13:29:25 +0700

    tạo giao diện đặt hàng thành công

--- routes/client/order.route.ts
+++ routes/client/order.route.ts
@@ -3,5 +3,6 @@ const router: Router = Router();
 import * as controller from "../../controllers/client/order.controller";
 
 router.post('/', controller.order);
+router.get('/success', controller.success);
 
 export const orderRoutes: Router = router;
