21abc390cddbd656bbaa953f072d1930cef3eefa
chison2707 <chiphansonzz17@gmail.com>
2025-02-25 16:02:34 +0700

    thêm mới nhóm quyền

--- controllers/admin/role.controller.ts
+++ controllers/admin/role.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Role from "../../models/role.model";
+import { systemConfig } from "../../config/system";
 
 //[GET] / admin/roles
 export const index = async (req: Request, res: Response) => {
@@ -20,4 +21,19 @@ export const create = async (req: Request, res: Response) => {
     res.render("admin/pages/roles/create", {
         pageTitle: "Danh sách nhóm quyền"
     });
+}
+
+//[ POST] / admin/roles/create
+export const createPost = async (req: Request, res: Response) => {
+    try {
+        await Role.create({
+            title: req.body.title,
+            description: req.body.description,
+        });
+        req.flash('success', 'Thêm nhóm quyền mới thành công');
+        res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+    } catch (error) {
+        req.flash('error', 'Thêm nhóm quyền mới thất bại');
+        res.redirect(`/${systemConfig.prefixAdmin}/roles`);
+    }
 }
--- routes/admin/role.route.ts
+++ routes/admin/role.route.ts
@@ -5,5 +5,6 @@ import * as controller from "../../controllers/admin/role.controller";
 
 router.get("/", controller.index);
 router.get("/create", controller.create);
+router.post("/create", controller.createPost);
 
 export const roleRoutes: Router = router;
