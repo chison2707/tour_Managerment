7619dd692226c635d9a836a4cbee73c6bd2def3e
chison2707 <chiphansonzz17@gmail.com>
2025-02-26 16:56:22 +0700

    đăng nhập trang admin

---
+++ middlewares/admin/auth.middleware.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import Role from "../../models/role.model";
+import { systemConfig } from "../../config/system";
+import adminAccount from "../../models/adminAccount.model";
+
+export const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.cookies.token) {
+        res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+        return;
+    } else {
+        const user = await adminAccount.findOne({
+            where: { token: req.cookies.token },
+            raw: true,
+            attributes: { exclude: ["password"] }
+        });
+
+        if (!user) {
+            res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+            return;
+        }
+        else {
+            const role = await Role.findOne({
+                where: { id: user["role_id"] },
+                attributes: ["title", "permissions"]
+            });
+
+            res.locals.user = user;
+            res.locals.role = role;
+            next();
+        }
+    }
+}
--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,22 +1,29 @@
 import { Express } from "express";
-import { categoryRoutes } from "./category.route";
 import { systemConfig } from "../../config/system";
+import * as authMiddleware from "../../middlewares/admin/auth.middleware";
+
+import { categoryRoutes } from "./category.route";
 import { tourRoutes } from "./tour.route";
 import { orderRoutes } from "./order.route";
 import { uploadRoutes } from "./upload.route"
 import { accountRoutes } from "./acccount.route"
 import { roleRoutes } from "./role.route"
+import { authRoutes } from "./auth.route"
+import * as authController from "../../controllers/admin/auth.controller"
 
 const adminRoutes = (app: Express): void => {
 
     const PATH_ADMIN = `/${systemConfig.prefixAdmin}`;
 
-    app.use(`${PATH_ADMIN}/categories`, categoryRoutes);
-    app.use(`${PATH_ADMIN}/tours`, tourRoutes);
-    app.use(`${PATH_ADMIN}/upload`, uploadRoutes);
-    app.use(`${PATH_ADMIN}/orders`, orderRoutes);
-    app.use(`${PATH_ADMIN}/accounts`, accountRoutes);
-    app.use(`${PATH_ADMIN}/roles`, roleRoutes);
+    app.get(PATH_ADMIN, authController.login);
+
+    app.use(`${PATH_ADMIN}/auth`, authRoutes);
+    app.use(`${PATH_ADMIN}/categories`, authMiddleware.requireAuth, categoryRoutes);
+    app.use(`${PATH_ADMIN}/tours`, authMiddleware.requireAuth, tourRoutes);
+    app.use(`${PATH_ADMIN}/upload`, authMiddleware.requireAuth, uploadRoutes);
+    app.use(`${PATH_ADMIN}/orders`, authMiddleware.requireAuth, orderRoutes);
+    app.use(`${PATH_ADMIN}/accounts`, authMiddleware.requireAuth, accountRoutes);
+    app.use(`${PATH_ADMIN}/roles`, authMiddleware.requireAuth, roleRoutes);
 
 };
 
