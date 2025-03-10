bef7493ca7e843022f17ea4e3251ea1497da9c68
chison2707 <chiphansonzz17@gmail.com>
2025-03-10 08:13:03 +0700

    phân quyền controller ql tk user

--- controllers/admin/user.controller.ts
+++ controllers/admin/user.controller.ts
@@ -23,53 +23,74 @@ export const index = async (req: Request, res: Response) => {
 
 //[GET] / admin/users/detail/:id
 export const detail = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const data = await User.findOne({
-        where: {
-            id: id,
-            deleted: false
-        },
-        raw: true
-    });
-    res.render("admin/pages/users/detail", {
-        pageTitle: "Chi tiết tài khoản user",
-        data: data
-    });
+    const permissions = res.locals.role.permissions;
+
+    if (!permissions.includes("user_view")) {
+        res.status(403).send("Bạn không có quyền xem quản lý tài khoản user");
+        return;
+    } else {
+        const id = req.params.id;
+        const data = await User.findOne({
+            where: {
+                id: id,
+                deleted: false
+            },
+            raw: true
+        });
+        res.render("admin/pages/users/detail", {
+            pageTitle: "Chi tiết tài khoản user",
+            data: data
+        });
+    }
 }
 
 //[PATCH] / admin/users/change-status/:status/:id
 export const changeStatus = async (req: Request, res: Response) => {
-    try {
-        const id = req.params.id;
-        const status = req.params.status;
-        await User.update({
-            status: status
-        }, {
-            where: {
-                id: id
-            }
-        });
-        req.flash("success", "Cập nhật trạng thái thành công");
-        res.redirect(`/${systemConfig.prefixAdmin}/users`);
-    } catch (error) {
-        req.flash("error", "Cập nhật trạng thái thất bại");
-        res.redirect(`/${systemConfig.prefixAdmin}/users`);
+    const permissions = res.locals.role.permissions;
+
+    if (!permissions.includes("user_edit")) {
+        res.status(403).send("Bạn không có quyền chỉnh sửa tài khoản user");
+        return;
+    } else {
+        try {
+            const id = req.params.id;
+            const status = req.params.status;
+            await User.update({
+                status: status
+            }, {
+                where: {
+                    id: id
+                }
+            });
+            req.flash("success", "Cập nhật trạng thái thành công");
+            res.redirect(`/${systemConfig.prefixAdmin}/users`);
+        } catch (error) {
+            req.flash("error", "Cập nhật trạng thái thất bại");
+            res.redirect(`/${systemConfig.prefixAdmin}/users`);
+        }
     }
 }
 
 //[DELETE] / admin/users/delete/:id
 export const deleteUser = async (req: Request, res: Response) => {
-    try {
-        const id = req.params.id;
-        await User.destroy({
-            where: {
-                id: id
-            }
-        });
-        req.flash("success", "Xóa tài khoản thành công");
-        res.redirect(`/${systemConfig.prefixAdmin}/users`);
-    } catch (error) {
-        req.flash("error", "Xóa tài khoản thất bại");
-        res.redirect(`/${systemConfig.prefixAdmin}/users`);
+    const permissions = res.locals.role.permissions;
+
+    if (!permissions.includes("user_delete")) {
+        res.status(403).send("Bạn không có quyền xóa tài khoản user");
+        return;
+    } else {
+        try {
+            const id = req.params.id;
+            await User.destroy({
+                where: {
+                    id: id
+                }
+            });
+            req.flash("success", "Xóa tài khoản thành công");
+            res.redirect(`/${systemConfig.prefixAdmin}/users`);
+        } catch (error) {
+            req.flash("error", "Xóa tài khoản thất bại");
+            res.redirect(`/${systemConfig.prefixAdmin}/users`);
+        }
     }
 }
